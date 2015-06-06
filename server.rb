c588733e886202995034232b6ae2f8dcb9f0c5aa
syzygy333 <brianjonashogue@gmail.com>
2015-06-05 20:02:17 -0400

    add button functionality, add views for success or failure, reorganize gets

--- server.rb
+++ server.rb
@@ -1,28 +1,43 @@
 require 'sinatra'
 require 'csv'
+require 'pry'
 
 get '/' do
+  button_roll = 1 + rand(10)
 
-def dice_roll
-  1 + rand(10)
+  if button_roll.even?
+    button1 = "/failure"
+    button2 = "/success"
+  else
+    button1 = "/success"
+    button2 = "/failure"
+  end
+
+  erb :index, locals: { button1: button1, button2: button2 }
 end
 
-descriptor1 = CSV.read('descriptor1.csv')
-common = CSV.read('common.csv')
-rare = CSV.read('rare.csv')
-epic = CSV.read('epic.csv')
-descriptor2 = CSV.read('descriptor2.csv')
+get '/success' do
+  loot_roll = 1 + rand(10)
 
-loot_roll = dice_roll
-rarity_key = { 1 => common, 2 => common, 3 => common, 4 => rare, 5 => rare, 6 => rare, 7 => rare, 8 => rare, 9 => rare, 10 => epic }
+  descriptor1 = CSV.read('descriptor1.csv')
+  common = CSV.read('common.csv')
+  rare = CSV.read('rare.csv')
+  epic = CSV.read('epic.csv')
+  descriptor2 = CSV.read('descriptor2.csv')
 
-color_key = { 1 => "common", 2 => "common", 3 => "common", 4 => "rare", 5 => "rare", 6 => "rare", 7 => "rare", 8 => "rare", 9 => "rare", 10 => "epic" }
-item_color = color_key[loot_roll]
+  rarity_key = { 1 => common, 2 => common, 3 => common, 4 => rare, 5 => rare, 6 => rare, 7 => rare, 8 => rare, 9 => rare, 10 => epic }
 
-chosen_noun = rarity_key[loot_roll][0].sample
-loot = "#{descriptor1[0].sample} #{chosen_noun} of #{descriptor2[0].sample}!"
+  color_key = { 1 => "common", 2 => "common", 3 => "common", 4 => "rare", 5 => "rare", 6 => "rare", 7 => "rare", 8 => "rare", 9 => "rare", 10 => "epic" }
+  item_color = color_key[loot_roll]
 
-  erb :index, locals: { loot: loot, item_color: item_color }
+  chosen_noun = rarity_key[loot_roll][0].sample
+  loot = "#{descriptor1[0].sample} #{chosen_noun} of #{descriptor2[0].sample}!"
+
+  erb :success, locals: { loot: loot, item_color: item_color }
+end
+
+get '/failure' do
+  erb :failure
 end
 
 # different colors on text to indicate rarity
@@ -34,3 +49,4 @@ end
 # and possibly equip items onto avatar
 # • when launch ends, user would have to relinquish items back into the pool,
 #   but their names would get attached to the item for sense of inheritance
+# • tailor descriptors to item rarity
