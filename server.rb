e11b2383c6f2d0d2b6d73a2f03d6e5154d948e9f
syzygy333 <brianjonashogue@gmail.com>
2015-06-15 16:01:24 -0400

    Reorganize, make roll happen once

--- server.rb
+++ server.rb
@@ -1,19 +1,38 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'csv'
-require 'pry'
+
+# ****** new organization requires server restart to get new item *******
 
 def random_url
   SecureRandom.urlsafe_base64(12)
 end
 
-url = random_url
-
 def roller
   1 + rand(10)
 end
 
+url = random_url
+roll = roller
+
+descriptor1 = CSV.read('lootdata/descriptor1.csv')
+common = CSV.read('lootdata/common.csv')
+rare = CSV.read('lootdata/rare.csv')
+epic = CSV.read('lootdata/epic.csv')
+descriptor2 = CSV.read('lootdata/descriptor2.csv')
+
+RARITY_KEY = { 1 => common, 2 => common, 3 => common, 4 => common, 5 => common, 6 => rare, 7 => rare, 8 => rare, 9 => epic, 10 => epic }
+
+COLOR_KEY = { 1 => "common", 2 => "common", 3 => "common", 4 => "common", 5 => "common", 6 => "rare", 7 => "rare", 8 => "rare", 9 => "epic", 10 => "epic" }
+
+item_color = COLOR_KEY[roll]
+item_attribute_1 = descriptor1[0].sample
+item_object = RARITY_KEY[roll][0].sample
+item_attribute_2 = descriptor2[0].sample
+
 get '/' do
-  if roller.even?
+
+  if roll.even?
     button1 = "/failure"
     button2 = "/#{url}"
   else
@@ -25,19 +44,8 @@ get '/' do
 end
 
 get "/#{url}" do
-  descriptor1 = CSV.read('descriptor1.csv')
-  common = CSV.read('common.csv')
-  rare = CSV.read('rare.csv')
-  epic = CSV.read('epic.csv')
-  descriptor2 = CSV.read('descriptor2.csv')
 
-  rarity_key = { 1 => common, 2 => common, 3 => common, 4 => common, 5 => common, 6 => rare, 7 => rare, 8 => rare, 9 => epic, 10 => epic }
-
-  color_key = { 1 => "common", 2 => "common", 3 => "common", 4 => "common", 5 => "common", 6 => "rare", 7 => "rare", 8 => "rare", 9 => "epic", 10 => "epic" }
-  item_color = color_key[roller]
-
-  chosen_noun = rarity_key[roller][0].sample
-  loot = "#{descriptor1[0].sample} #{chosen_noun} of #{descriptor2[0].sample}!"
+  loot = "#{item_attribute_1} #{item_object} of #{item_attribute_2}!"
 
   erb :success, locals: { loot: loot, item_color: item_color }
 end
@@ -49,10 +57,17 @@ end
 
 
 # future implementations
-# • a Take It button on the success page that triggers removal from the list and redirect to home
-# • randomize page names so you can't keep refreshing on 'success' without clicking the correct button
-# • remove looted items (words) from CSVs for uniqueness
+# • Take It button should trigger item's removal from the list and insertion into loot database
 # • would be great if user could collect items if signed in
 # and possibly equip items onto avatar
 # • when launch ends, user would have to relinquish items back into the pool, but their names would get attached to the item for sense of inheritance. this would mean adding a 'legendary' rarity.
 # • tailor descriptors to item rarity
+
+# def db_connection
+#   begin
+#     connection = PG.connect(dbname: "korning")
+#     yield(connection)
+#   ensure
+#     connection.close
+#   end
+# end
